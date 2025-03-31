1f771e7a0fe978e684bd5f158e78c184f4ecd235
Abhishek khati chhetri <abhichhetri@outlook.com>
2025-04-01 00:23:11 +0300

    Add padding to Section component for improved layout

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,5 @@
 const Section = ({ children, className }) => {
-  return <section className={`py-10 ${className}`}>{children}</section>;
+  return <section className={`py-10 px-8 ${className}`}>{children}</section>;
 };
 
 export { Section };
