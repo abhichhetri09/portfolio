98a8452d5058a2b93252c96a51f4cc093d52614e
Abhishek khati chhetri <abhichhetri@outlook.com>
2026-01-16 21:37:59 +0200

    Update Footer component styling for improved visibility and aesthetics; change background color, text color, and add hover effects to social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,20 @@ const Footer = () => {
   const { pathname } = useLocation();
 
   return (
-    <footer className={pathname === "/" ? "hidden" : "block bg-slate-800 "}>
-      <div className="container py-8 flex flex-col md:flex-row justify-center border-t border-t-white/10 md:justify-between gap-4">
-        <p className="text-white text-center">
-          © 2025 <strong>Abhishek Khati Chhetri</strong>. All rights reserved.
+    <footer className={pathname === "/" ? "hidden" : "block bg-slate-100 shadow-md"}>
+      <div className="container py-8 flex flex-col md:flex-row justify-center border-t border-t-slate-300 md:justify-between gap-4">
+        <p className="text-slate-700 text-center">
+          © 2025 <strong className="text-slate-800">Abhishek Khati Chhetri</strong>. All rights reserved.
         </p>
 
         <div className="flex gap-3 justify-center items-center">
           {socialLinks.map((link) => (
-            <Link key={link.name} to={link.link} target="_blank">
+            <Link 
+              key={link.name} 
+              to={link.link} 
+              target="_blank"
+              className="hover:opacity-70 transition-opacity"
+            >
               <img
                 src={link.iconUrl}
                 alt={link.name}
